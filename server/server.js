293b804ec497d8248d1963bce4d3d2be9c76ac2b
Thomas Nemeth <thonem10@gmail.com>
2022-02-28 13:10:34 +0100

    added start script to root package

--- server/server.js
+++ server/server.js
@@ -2,12 +2,17 @@ import express from "express";
 
 const app = express();
 
-app.get("/question/random", (req, res) => {
+app.get("/api/question", (req, res) => {
   res.json({
+    id:"5",
+    category:"Mat",
     question: "hva gjør du?",
     answer: "dummy svar",
   });
 });
+
+//todo
+//app.post("/api/question")
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Server started on: http://localhost:${server.address().port}`);
