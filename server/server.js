be207c46be075d89e89fd751517d448a16b95358
Thomas Nemeth <thonem10@gmail.com>
2022-03-02 19:34:06 +0100

    found the reason for the server to crash

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 import express from "express";
 import * as path from "path";
 import bodyParser from "body-parser";
-import { isCorrectAnswer, Questions, randomQuestion } from "./questions";
+import { isCorrectAnswer, Questions, randomQuestion } from "./questions.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
@@ -73,6 +74,7 @@ app.post("/api/question", (req, res) => {
   //tar inn id og answer -> false ? true
   const { id, answer } = res.body;
 });
+
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Server started on: http://localhost:${server.address().port}`);
