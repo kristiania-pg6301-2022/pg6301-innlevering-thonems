c0a0d60a92dee2fbc991c5be944ec8d050d11a0c
Thomas Nemeth <thonem10@gmail.com>
2022-03-02 20:01:58 +0100

    rename

--- client/__tests__/client.test.jsx
+++ client/__tests__/client.test.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { MemoryRouter } from "react-router-dom";
-import { Quiz, QuestionContext, ShowQuestion, FrontPage } from "../Quiz";
+import { Quiz, QuestionContext, ShowQuestion, FrontPage } from "../Quiz.jsx";
 import { Simulate } from "react-dom/test-utils";
 
 const questionNotRandom = {
