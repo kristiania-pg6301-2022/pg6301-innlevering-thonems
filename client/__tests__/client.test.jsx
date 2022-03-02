6f3f742f741a30d14faf6aa613c260928df1667b
Thomas Nemeth <thonem10@gmail.com>
2022-03-02 13:08:59 +0100

    installed @babel* to get tests running locally 🔴

--- client/__tests__/client.test.jsx
+++ client/__tests__/client.test.jsx
@@ -1,5 +1,16 @@
+import {ShowQuestion} from "../Quiz";
+import {render} from "react-dom";
+
+
 describe("", () => {
   it("Test if test runs on client", () => {
     expect(3).toBe(3);
+  });
+
+  it("Should render question", () => {
+      const element = document.createElement("div");
+
+      render(<ShowQuestion/>, element);
+      expect(element.innerHTML).toMatchSnapshot();
   });
 });
