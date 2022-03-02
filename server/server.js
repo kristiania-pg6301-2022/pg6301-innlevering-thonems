76c780118f68d7987ca5b8a746a17d16f4a00e18
Thomas Nemeth <thonem10@gmail.com>
2022-03-02 20:00:15 +0100

    testing

--- server/server.js
+++ server/server.js
@@ -75,7 +75,6 @@ app.post("/api/question", (req, res) => {
   const { id, answer } = res.body;
 });
 
-
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Server started on: http://localhost:${server.address().port}`);
 });
